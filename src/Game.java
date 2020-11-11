96d16523c9e736fe2ae907b9e8039d3d7870a863
syed <umaranwar707@gmail.com>
2020-11-11 14:03:39 +0530

    Added:roll the dice to get the number between 1 to 6

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,11 @@ public class Game {
     int position=0;
 	public static void main(String[] args) {
 		System.out.println("only one user can play");
+		rolls_dice();
 
 	}
-
+    public static void rolls_dice() {
+    	int dice=(int)(Math.floor(Math.random()*10)%6+1);
+    	System.out.println("the dice is "+dice);
+    }
 }
