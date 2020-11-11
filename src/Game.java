6ae8b71ce1fc8471d047e6ecb652b25fc217ff87
syed <umaranwar707@gmail.com>
2020-11-11 15:17:28 +0530

    Added:continew till position will be 100

--- src/Game.java
+++ src/Game.java
@@ -13,18 +13,22 @@ public class Game {
     }
     public static void option() {
     	int position=0;
-    	int luck=(int)(Math.floor(Math.random()*10)%3+1);
-    	int dice=rolls_dice();
-    	System.out.println("the dice is "+dice);
-    	switch (luck) {
-    	case 1:System.out.println("the position is "+position);
+    	while (position<=100) {
+    		int luck=(int)(Math.floor(Math.random()*10)%3+1);
+    		int dice=rolls_dice();
+    		System.out.println("the dice is "+dice);
+    		System.out.println("the luck is "+luck);
+    		switch (luck) {
+    		case 1:System.out.println("the position is "+position);
     			break;
-    	case 2:	position+=dice;
+    		case 2:	position+=dice;
     			System.out.println("the position is "+position);
     			break;
-    	case 3:position=position-dice;
+    		case 3:position=position-dice;
     			System.out.println("the postition is "+position);
+    			if(position<0){position=0;};
     			break;
+    		}	
     	}
     }
     
